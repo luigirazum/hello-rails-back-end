25756bdcfb9281c9775c6aa31c34d31f1cedb545
Luis Zubia <soyzubia@gmail.com>
2024-03-09 15:10:06 -0700

    Make the api respond to 'api/v1/greetings/random' and return a Greeting

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -5,7 +5,14 @@ module Api
         render plain: 'This is the API, back end, the available entry point is greetings/random', status: :ok
       end
 
-      def random; end
+      def random
+        @message = Greeting.all.sample
+        if Greeting.any?
+          render json: @message
+        else
+          render plain: 'There are no messages', status: :not_found
+        end
+      end
     end
   end
 end
