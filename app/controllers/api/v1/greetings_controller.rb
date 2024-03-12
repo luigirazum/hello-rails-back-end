b5dece36aedb2bc68309889d0940723ced642fa3
Luis Zubia <soyzubia@gmail.com>
2024-03-12 02:29:34 -0700

    Refactor Greetings controller: Add RESPONSE_HEADERS and set status to unprocessable_entity when there are no Greeting records

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,16 +1,27 @@
 module Api
   module V1
     class GreetingsController < ApplicationController
+      RESPONSE_HEADERS = { 'Access-Control-Allow-Origin': 'http://localhost:3001',
+                           'Content-Type': 'application/json; charset=utf-8' }.freeze
+
       def index
-        render plain: 'This is the API, back end, the available entry point is greetings/random', status: :ok
+        render json: {
+          data: { message: 'This is the API, back end, the available entry point is greetings/random' },
+          headers: RESPONSE_HEADERS
+        }, status: :ok
       end
 
       def random
         @message = Greeting.all.sample
         if Greeting.any?
-          render json: @message
+          render json: {
+            data: @message,
+            headers: RESPONSE_HEADERS
+          }, status: :ok
         else
-          render plain: 'There are no messages', status: :not_found
+          render json: {
+            error: 'There are no messages'
+          }, status: :unprocessable_entity
         end
       end
     end
